17ba04116f5b12122aa652fb65057a02d9eb9ad9
delasylviaa <delasylviaa@gmail.com>
2024-09-21 00:01:01 +0700

    allah

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create User</title>
-    <link rel="stylesheet" href="{{asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="styles.css"> <!-- Hubungkan ke CSS eksternal -->
 </head>
 <body>
     <div class="container">
-        <img src="{{ asset('assets/img/kucing.png') }}"alt="cute kitten" class=".cute-kitten">
+        <h1>Create User</h1>
+        <img src="kucing.png" alt="Cute Kitten" class="cute-kitten"> <!-- Gambar kucing -->
         <form>
-            <label for="username">Username:</label>
+            <label for="username">Nama:</label>
             <input type="text" id="username" name="username" placeholder="Enter your username">
 
-            <label for="email">Email:</label>
+            <label for="email">NPM:</label>
             <input type="email" id="email" name="email" placeholder="Enter your email">
 
-            <label for="password">Password:</label>
+            <label for="password">Kelas:</label>
             <input type="password" id="password" name="password" placeholder="Enter your password">
 
             <button type="submit" class="submit-btn">Create User</button>
