ab22465ae8af3494985c844161b6250ad8d7abcf
delasylviaa <delasylviaa@gmail.com>
2024-09-21 00:13:47 +0700

    ppppp

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -3,22 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create User</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Hubungkan ke CSS eksternal -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
     <div class="container">
         <h1>Create User</h1>
-        <img src="kucing.png" alt="Cute Kitten" class="cute-kitten"> <!-- Gambar kucing -->
+        <img src="{{ asset('assets/imgkucing.png) }}" alt="Cute Kitten" class=".cute-kitten"> <!-- Gambar kucing -->
         <form>
-            <label for="username">Nama:</label>
-            <input type="text" id="username" name="username" placeholder="Enter your username">
+            <label for="nama">Nama:</label>
+            <input type="text" id="nama" name="nama" placeholder="Enter your name">
 
-            <label for="email">NPM:</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email">
+            <label for="npm">NPM:</label>
+            <input type="text" id="npm" name="npm" placeholder="Enter your npm">
 
-            <label for="password">Kelas:</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password">
+            <label for="kelas">Kelas:</label>
+            <input type="kelas" id="kelas" name="kelas" placeholder="Enter your class">
 
             <button type="submit" class="submit-btn">Create User</button>
         </form>
