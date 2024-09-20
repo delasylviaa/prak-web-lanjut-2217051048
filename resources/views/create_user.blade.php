062985fd3cb2bee02e8773a275b23111d75e6897
delasylviaa <delasylviaa@gmail.com>
2024-09-20 23:39:54 +0700

    yyyy

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create User</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Hubungkan dengan file CSS eksternal -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css') }}">
 </head>
 <body>
     <div class="container">
